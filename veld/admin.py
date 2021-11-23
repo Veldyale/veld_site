f5bfb214b5a76afedca03e92078cd2fc327554e8
veldyale <veldyale@icloud.com>
2021-11-23 21:16:30 +0400

    created models, admin  and adminuser

--- veld/admin.py
+++ veld/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "service", "datetime",)
+    list_display_links = ("id", "customer", "service", "datetime",)
+    # list_filter = ("is_published", "time_create",)
+    # search_fields = ("id", "title", "time_create",)
+    # list_editable = ("is_published",)
+    # prepopulated_fields = {"slug": ("title",)}
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "phone_number", "gender",)
+    list_display_links = ("id", "name",)
+    # list_filter = ("id", "name",)
+    # search_fields = ("id", "name",)
+    # prepopulated_fields = {"slug": ("name",)}
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "durations_service_time",)
+    list_display_links = ("id", "name",)
+    # list_filter = ("id", "name",)
+    # search_fields = ("id", "name",)
+    # prepopulated_fields = {"slug": ("name",)}
+
+
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display_links = ("name",)
+    # list_filter = ("id", "name",)
+    # search_fields = ("id", "name",)
+    # prepopulated_fields = {"slug": ("name",)}
+
+
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display_links = ("name",)
+    # list_filter = ("id", "name",)
+    # search_fields = ("id", "name",)
+    # prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Gender, GenderAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Master, MasterAdmin)
--- veld/models.py
+++ veld/models.py
@@ -1,29 +1,69 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 
 class Appointment(models.Model):
-    datetime = models.DateTimeField(verbose_name='Дата и время')
-    service = models.ForeignKey('Service', on_delete=models.PROTECT, verbose_name='Пол')
+    service = models.ForeignKey('Service', on_delete=models.PROTECT, verbose_name='Услуга')
     customer = models.ForeignKey('Customer', on_delete=models.PROTECT, verbose_name='Клиент')
     master = models.ForeignKey('Master', on_delete=models.PROTECT, verbose_name='Мастер')
-    durations_service_time = models.ForeignKey('Service', on_delete=models.PROTECT, verbose_name='Длительность')
+    datetime = models.DateTimeField(verbose_name='Дата и время')
+    # durations_service_time = models.ForeignKey('Service', on_delete=models.PROTECT, verbose_name='Длительность')
+
+    # def __str__(self):
+    #     return self.service
+
+    class Meta:
+        ordering = ('datetime',)
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Имя")
     gender = models.ForeignKey('Gender', on_delete=models.PROTECT, verbose_name='Пол')
-    phone_number = PhoneNumberField()
+    phone_number = models.CharField(max_length=12, verbose_name="Номер телефона")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
 
 
 class Gender(models.Model):
     name = models.CharField(max_length=30, verbose_name='Пол')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Пол"
+        verbose_name_plural = "Пол"
+
 
 class Service(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    durations_service_time = models.PositiveIntegerField(verbose_name='Длительность [мин]')
+    price = models.DecimalField(max_digits=9, decimal_places=0, verbose_name='Стоимость [₽]')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
 
 
 class Master(models.Model):
-    pass
+    name = models.CharField(max_length=255, verbose_name="Мастер")
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
 
